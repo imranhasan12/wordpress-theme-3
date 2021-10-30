df1ff8c9e7b703f225bb16de68c7dc51785d8e30
imranhasan12 <73755547+imranhasan12@users.noreply.github.com>
2021-10-31 01:10:51 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+   <footer class="container-fluid">
+        <div class="row">
+            <p class="col-lg-9 col-md-8 mb-5 mb-md-0">Copyright &copy; 2020 <span class="tm-text-primary">Business Oriented</span> 
+                - designed by <a href="https://templatemo.com" class="tm-link-primary">TemplateMo</a></p>
+            <div class="col-lg-3 col-md-4 text-right">
+                <a href="https://fb.com/templatemo" class="tm-social-link"><i class="fab fa-facebook fa-2x tm-social-icon"></i></a>
+                <a href="https://twitter.com" class="tm-social-link"><i class="fab fa-twitter fa-2x tm-social-icon"></i></a>
+                <a href="https://linkedin.com" class="tm-social-link"><i class="fab fa-linkedin fa-2x tm-social-icon"></i></a>
+            </div>
+        </div>
+    </footer>
+  <script>
+        $(document).ready(function () {
+            $('#parallax-1').parallax({ imageSrc: 'img/biz-oriented-header.jpg' });
+            $('#parallax-2').parallax({ imageSrc: 'img/biz-oriented-footer.jpg' });
+        });
+    </script>
+   
+  
+	<?php wp_footer();?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Business Oriented CSS Template</title>
+    /
+	
+	
+	<?php wp_head();?>
+</head>
+<!--
+
+TemplateMo 549 Business Oriented
+
+https://templatemo.com/tm-549-business-oriented
+
+-->
+<body>
+    <div id="parallax-1" class="parallax-window" data-parallax="scroll" data-image-src="<?php echo get_template_directory_uri();?>/img/biz-oriented-header.jpg">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="tm-logo">
+                        <i class="fas fa-industry fa-5x mr-5"></i>
+                        <span class="text-uppercase tm-logo-text">Business Oriented</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tm-nav-container-outer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg" id="tm-main-nav">
+                        <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbar-nav" 
+                            aria-controls="navbar-nav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="dark-blue-text"><i class="fas fa-bars"></i></span>
+                        </button>
+                        <div class="collapse navbar-collapse tm-nav" id="navbar-nav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link tm-nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tm-nav-link" href="Services">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tm-nav-link" href="about.html">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tm-nav-link" href="contact.html">Contact</a>
+                                </li>
+                            </ul>                            
+                        </div>                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
